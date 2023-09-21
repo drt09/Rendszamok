2f45b0fd022aa41734f77ac4fc5d5e5a1adbe776
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-09-21 11:25:33 +0200

    Kész kód

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function Tarol()
 {
     var obj = {
         rendszam: document.getElementById("rendszam").value,
-        uzemanyag: document.getElementById("uzemselect").value,
+        uzemanyag: document.getElementById("uzemSelect").value,
         elsotulajdonos: document.getElementById("elsoTul").checked,
         ajtoharom: document.getElementById("harom").checked,
         ajtoot: document.getElementById("ot").checked
@@ -23,19 +23,41 @@ function Tarol()
 
 function Statisztika()
 {
-    var b = 0, d = 0, e = 0, atlag = 0, regi = 0, uj = 0, ajto = 0
-    document.getElementById("statisztika").innerHTML += "<table><tr><td>"
-    document.getElementById("statisztika").innerHTML +=
-    "Üzemanyag szerint</td><td>Benzin</td><td>Dízel</td><td>Elektromos</td><td></td></tr>"
-    document.getElementById("statisztika").innerHTML += 
-    "<tr><td>"+b+"</td><td>"+d+"</td><td>"+e+"</td></tr>"
-    document.getElementById("statisztika").innerHTML += 
-    "<tr><td>Első tulajdonosok százalékos aránya</td><td>"+atlag+"</td></tr>"
-    document.getElementById("statisztika").innerHTML += 
-    "<tr><td>Rendszámok megoszlása</td><td>Régi rendszám</td><td>Új rendszám</td></tr>"
-    document.getElementById("statisztika").innerHTML += 
-    "<tr><td></td><td>"+regi+"</td><td>"+uj+"</td></tr>"
-    document.getElementById("statisztika").innerHTML +=
-    "<tr><td>Melyik népszerűbb?</td><td>"+ajto+"</td></tr>"
-    document.getElementById("statisztika").innerHTML += "</table>"
+    var b = 0, d = 0, e = 0, atlag = 0, regi = 0, uj = 0, ajto = "", elsotulajdonos = 0, ajto3 = 0, ajto5 = 0
+    var s = ""
+
+    for (let i = 0; i < t.length; i++) 
+    {
+        if (t[i].uzemanyag == "benzin") b++
+        else if (t[i].uzemanyag == "dizel") d++
+        else if (t[i].uzemanyag == "elektromos") e++
+        if (t[i].elsotulajdonos)
+        {
+            elsotulajdonos++    
+        }
+        atlag = Math. round((elsotulajdonos/t.length)*100)
+        if (t[i].ajtoharom) ajto3++
+        else if (t[i].ajtoot) ajto5++
+        if (ajto3 < ajto5) ajto = "Az 5 ajtós modell"
+        else ajto = "A 3 ajtós modell"
+        if (t[i].rendszam.length >= 7) uj++
+        else regi++
+    }
+
+    s += "<table id='statisztikatable'><tr><td>"
+    s +=
+    "<b>Üzemanyag szerint</b></td><td>Benzin</td><td>Dízel</td><td>Elektromos</td><td></td></tr>"
+    s += 
+    "<tr><td></td><td>"+b+"</td><td>"+d+"</td><td>"+e+"</td></tr>"
+    s += 
+    "<tr><td><b>Első tulajdonosok százalékos aránya</b></td><td></td><td>"+atlag+"%</td></tr>"
+    s += 
+    "<tr><td><b>Rendszámok megoszlása</b></td><td>Régi rendszám</td><td></td><td>Új rendszám</td></tr>"
+    s += 
+    "<tr><td></td><td>"+regi+"</td><td></td><td>"+uj+"</td></tr>"
+    s +=
+    "<tr><td><b>Melyik népszerűbb?</b></td><td></td><td>"+ajto+"</td></tr>"
+    s += "</table>"
+    document.getElementById("statisztika").innerHTML = s
 }
+
